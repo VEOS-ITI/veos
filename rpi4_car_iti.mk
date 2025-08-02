af7cfd6450d06a5634adc2f6d59d42775e7fd2ed
Yara-Abdelrahem <yaraomran961@gmail.com>
2025-08-02 18:30:55 +0300

    make file

---
+++ rpi4_car_iti.mk
@@ -0,0 +1,8 @@
+# PRODUCT_PACKAGES += 
+
+
+# BOARD_SEPOLICY_DIRS += \
+#     vendor/Aidl_Service/sepolicy
+
+# DEVICE_FRAMWORK_COMPATIBILITY_MATRIX_FILE += \
+#     vendor/Aidl_Service/compatibility_matrix.xml
